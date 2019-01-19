d4097d7302cb62a6d0428937cb4f3d14ae15f613
Breno Vinícios <breno@poppinapp.com.br>
2019-01-19 15:16:00 -0200

    Build: Folders structure added

---
+++ Weather/Weather/Controllers/ActualWeather/WeatherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.swift
+//  Weather
+//
+//  Created by Breno Vinícios on 19/01/19.
+//  Copyright © 2019 breno.vinicios. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+}
---
+++ Weather/Weather/Controllers/ActualWeatherViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ActualWeatherViewController.swift
+//  Weather
+//
+//  Created by Breno Vinícios on 19/01/19.
+//  Copyright © 2019 breno.vinicios. All rights reserved.
+//
+
+class ActualWeatherViewController: WeatherViewController {
+    
+}
