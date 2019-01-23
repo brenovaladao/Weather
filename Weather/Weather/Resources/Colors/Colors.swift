e8a11e11463aeab2ce6f692023a18ca9fe638698
Breno Vinícios <breno@poppinapp.com.br>
2019-01-22 22:02:13 -0200

    All views created for actual weather screen

--- Weather/Weather/Resources/Colors/Colors.swift
+++ Weather/Weather/Resources/Colors/Colors.swift
@@ -12,30 +12,34 @@ extension UIColor {
 
     /// RGB: 47, 145, 255
     /// Hexa: 2F91FF
-    class var weatherBlue: UIColor { return UIColor(red: 47/255.0, green: 145/255.0, blue: 255/255.0, alpha: 1.0) }
+    static let weatherBlue = UIColor(red: 47/255.0, green: 145/255.0, blue: 255/255.0, alpha: 1.0)
     
     /// RGB: 51, 51, 51
     /// Hexa: 333333
-    static var weatherDarkGray: UIColor { return UIColor(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1.0) }
+    static let weatherDarkGray = UIColor(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1.0)
 
     /// RGB: 222, 222, 222
     /// Hexa: DEDEDE
-    static var weatherLightGray: UIColor { return UIColor(red: 222/255.0, green: 222/255.0, blue: 222/255.0, alpha: 1.0) }
+    static let weatherLightGray = UIColor(red: 222/255.0, green: 222/255.0, blue: 222/255.0, alpha: 1.0)
 
+    /// RGB: 255, 136, 71
+    /// Hexa: FF8847
+    static let weatherOrange = UIColor(red: 255/255.0, green: 136/255.0, blue: 71/255.0, alpha: 1.0)
+    
     /// RGB: 255, 143, 222
     /// Hexa: FF8FDE
-    static var weatherPink: UIColor { return UIColor(red: 255/255.0, green: 143/255.0, blue: 222/255.0, alpha: 1.0) }
-
+    static let weatherPink = UIColor(red: 255/255.0, green: 143/255.0, blue: 222/255.0, alpha: 1.0)
+    
     /// RGB: 31, 162, 147
     /// Hexa: 1FA22F
-    static var weatherGreen: UIColor { return UIColor(red: 31/255.0, green: 162/255.0, blue: 147/255.0, alpha: 1.0) }
+    static let weatherGreen = UIColor(red: 31/255.0, green: 162/255.0, blue: 147/255.0, alpha: 1.0)
 
     /// RGB: 255, 204, 51
     /// Hexa: FFCC33
-    static var weatherYellow: UIColor { return UIColor(red: 255/255.0, green: 204/255.0, blue: 51/255.0, alpha: 1.0) }
+    static let weatherYellow = UIColor(red: 255/255.0, green: 204/255.0, blue: 51/255.0, alpha: 1.0)
 
     /// RGB: 215, 55, 18
     /// Hexa: D73712
-    static var weatherRed: UIColor { return UIColor(red: 255/255.0, green: 55/255.0, blue: 18/255.0, alpha: 1.0) }
+    static let weatherRed = UIColor(red: 255/255.0, green: 55/255.0, blue: 18/255.0, alpha: 1.0)
     
 }
---
+++ Weather/Weather/Resources/Strings/Strings.swift
@@ -0,0 +1,20 @@
+//
+//  Strings.swift
+//  Weather
+//
+//  Created by Breno Vinícios on 22/01/19.
+//  Copyright © 2019 breno.vinicios. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    // MARK: Actual Weather Strings
+    
+    static let shareButtonTitle = NSLocalizedString("Share", comment: "Share button title")
+    
+    // MARK: Forecast Strings
+
+    // MARK: Views
+}
